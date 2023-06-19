c01460a10bae03f4cbaa6a70472375679a5c9b4c
dymitrbaranowski <dmitribaranowski51@gmail.com>
2023-06-19 21:47:02 +0300

    fox6

--- js/task-06.js
+++ js/task-06.js
@@ -1,22 +1,36 @@
-{/* <input
+/* <input
   type="text"
   id="validation-input"
   data-length="6"
   placeholder="Please enter 6 symbols"
-/>; */}
+/>; */
 
 
-const input = document.querySelector("#validation-input");
-const dataLength = input.getAttribute('data-length');
-console.log(input)
-input.addEventListener('blure', handlerCheckInput)
+// const input = document.querySelector("#validation-input");
+// const dataLength = input.getAttribute('data-length');
+// console.log(input)
+// input.addEventListener('blure', handlerCheckInput)
 
-function handlerCheckInput(evt) {
-  const currentValue = evt.currentTarget.value;
-  if (Number(datalength) === Number(currentValue.length)) {
-    input.classList.add('valid')
+// function handlerCheckInput(evt) {
+//   // const currentValue = evt.currentTarget.value;
+//   if (Number('dataLength') === Number(evt.currentValue.length)) {
+//     input.classList.add('valid')
+//   } else {
+//     input.classList.add('invalid')
+//   }
+  
+// }
+
+const inputEl = document.querySelector('#validation-input')
+const dataLength = inputEl.getAttribute('data-length');
+const inputValueLength = inputEl.value.length;
+
+const checkInputValueLength = () => {
+  if (Number(dataLength) === Number(inputValueLength)) {
+    inputEl.classList.add("valid")
   } else {
-    input.classList.add('invalid')
+    inputEl.classList.add("invalid")
   }
 }
-console.log()
+inputEl.addEventListener("blur", checkInputValueLength)
+console.log(inputValueLength);
